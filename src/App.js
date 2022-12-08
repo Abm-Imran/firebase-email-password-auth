8687464dcacc2efe87b9e849d376d77d3088a63b
unknown <Abm-Imran>
2022-12-08 15:30:57 +0600

    Create new user using email and password done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from './firebase.init';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+
+const auth = getAuth(app);
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleFormSubmit = (e) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        console.log(result.user);
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error);
+      })
+    e.preventDefault();
+    setEmail('');
+    setPassword('');
+
+  }
+
+  const handleEmailChange = (e) => {
+    // console.log(e.target.value);
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    // console.log(e.target.value);
+    setPassword(e.target.value);
+  }
   return (
-    <div className="App">
-      
+    <div className="">
+      <Form onSubmit={handleFormSubmit} className='w-50 mx-auto mt-2'>
+        <h2 className='text-primary' >Please Register</h2>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailChange} type="email" placeholder="Enter email" required />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePasswordChange} type="password" placeholder="Password" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <p>{error}</p>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
